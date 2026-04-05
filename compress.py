73e2813f22c2afdf4cde3b96f50e1984729fd76c
Linus <laei@zhaw.ch>
2026-04-05 13:04:27 +0200

    minified json added

--- compress.py
+++ compress.py
@@ -13,8 +13,11 @@ with open('chrome_extensions.json', 'r', encoding='utf-8') as f:
 with open('chrome_extensions_mini.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=None, separators=(',', ':'))
 
+# Sort the data by 'id' before writing to CSV
+sorted_data = sorted(data, key=lambda x: x.get('id', ''))
+
 with open('chrome_extensions.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerow(['id', 'name'])
-    for item in data:
+    for item in sorted_data:  # Use sorted_data instead of data
         writer.writerow([item.get('id', ''), item.get('name', '')])
